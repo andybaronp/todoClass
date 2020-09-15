07dcda846aec6933352dfe6ed22ca23291f903ed
Jhoandris Baron <andybaronp@gmail.com>
2020-09-15 11:39:16 -0500

    Builld

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import './styles.css';
+
+
 import  {Todo,TodoList } from './classes'
 import {crearTodoHtml} from './js/componentes'
-import './styles.css';
 
 
 
 export const todoList = new TodoList();
-
-
 todoList.todos.forEach(crearTodoHtml);
+
+
 
